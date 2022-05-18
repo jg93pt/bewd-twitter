423cd86abc14ceded7f1b9d480c6bd1fcf671036
jg93pt <jorgebenf@gmail.com>
2022-05-18 15:18:50 +0100

    created db users, sessions, attributes, models validations, post routes, controllers create functions

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_05_18_135320) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
+    t.datetime "timestamps"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.datetime "timestamps"
+  end
 
 end
